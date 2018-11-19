a89aaf5b46f32b2ee1a0a82c6bea315ef2694543
yangjc <bupt_yjc@163.com>
2018-11-19 10:06:55 +0800

    sync generate

--- generate.py
+++ generate.py
@@ -26,12 +26,12 @@ def define_model(name, embed_mat, seq_len, mode):
     else:
         state = Input(shape=(embed_len,))
     embed_input = embed(input)
-    func = map_item('_'.join([name, mode]), funcs)
+    arch = map_item('_'.join([name, mode]), archs)
     if mode == 'decode':
-        output = func(embed_input, state, vocab_num)
+        output = arch(embed_input, state, vocab_num)
         return Model([input, state], output)
     else:
-        output = func(embed_input)
+        output = arch(embed_input)
         return Model(input, output)
 
 
@@ -48,8 +48,78 @@ def ind2word(word_inds):
     return ind_words
 
 
+def check(probs, cand):
+    max_probs, max_inds = list(), list()
+    sort_probs = -np.sort(-probs)
+    sort_inds = np.argsort(-probs)
+    for prob, ind in zip(list(sort_probs), list(sort_inds)):
+        if ind not in skip_inds:
+            max_probs.append(prob)
+            max_inds.append(ind)
+        if len(max_probs) == cand:
+            break
+    return max_probs, max_inds
+
+
+def sample(decode, state, cand):
+    sent2 = bos
+    next_word, count = '', 0
+    while next_word != eos:
+        count = count + 1
+        sent2 = ' '.join([sent2, next_word])
+        seq2 = word2ind.texts_to_sequences([sent2])[0]
+        pad_seq2 = pad_sequences([seq2], maxlen=seq_len, padding='post', truncating='post')
+        step = min(count - 1, seq_len - 1)
+        probs = decode.predict([pad_seq2, state])[0][step]
+        max_probs, max_inds = check(probs, cand)
+        if max_inds[0] == word_inds[eos]:
+            next_word = eos
+        else:
+            max_probs = max_probs / np.sum(max_probs)
+            next_word = ind_words[choice(max_inds, p=max_probs)]
+    return sent2[2:]
+
+
+def search(decode, state, cand):
+    bos_ind = [word_inds[bos]]
+    pad_bos = pad_sequences([bos_ind], maxlen=seq_len, padding='post', truncating='post')
+    logs = np.log(decode.predict([pad_bos, state])[0][0])
+    max_logs, max_inds = check(logs, cand)
+    sent2s, log_sums = [bos] * cand, list(max_logs)
+    next_words = [ind_words[ind] for ind in list(max_inds)]
+    eos_sent2s, count = list(), 1
+    while cand > 0:
+        log_mat, ind_mat = list(), list()
+        count = count + 1
+        for sent2, next_word, log_sum in zip(sent2s, next_words, log_sums):
+            if next_word != eos:
+                sent2 = ' '.join([sent2, next_word])
+                seq2 = word2ind.texts_to_sequences([sent2])[0]
+                pad_seq2 = pad_sequences([seq2], maxlen=seq_len, padding='post', truncating='post')
+                step = min(count - 1, seq_len - 1)
+                logs = np.log(decode.predict([pad_seq2, state])[0][step])
+                max_logs, max_inds = check(logs, cand)
+                max_logs = (max_logs + log_sum) / count
+                log_mat.append(max_logs)
+                ind_mat.append(max_inds)
+        max_logs = -np.sort(-np.array(log_mat), axis=None)[:cand]
+        next_sent2s, next_words, log_sums = list(), list(), list()
+        for i in range(len(max_logs)):
+            args = np.where(log_mat == max_logs[i])
+            sent_arg, ind_arg = int(args[0]), int(args[1])
+            next_word = ind_words[ind_mat[sent_arg, ind_arg]]
+            if next_word == eos:
+                cand = cand - 1
+                eos_sent2s.append(sent2s[sent_arg])
+            else:
+                next_sent2s.append(sent2s[sent_arg])
+                next_words.append(ind_words[ind_mat[sent_arg][ind_arg]])
+                log_sums.append(log_mat[sent_arg][ind_arg])
+        sent2s = next_sent2s
+    return sent2s[0][2:]
+
+
 seq_len = 100
-max_len = 200
 
 bos, eos = '*', '#'
 
@@ -64,12 +134,17 @@ with open(path_word2ind, 'rb') as f:
     word2ind = pk.load(f)
 word_inds = word2ind.word_index
 
+skip_inds = [0, word_inds['oov']]
+
 ind_words = ind2word(word_inds)
 
-funcs = {'s2s_encode': s2s_encode,
+archs = {'s2s_encode': s2s_encode,
          's2s_decode': s2s_decode,
          'att_encode': att_encode,
          'att_decode': att_decode}
+
+funcs = {'sample': sample,
+         'search': search}
 
 paths = {'s2s': 'model/s2s.h5',
          'att': 'model/att.h5'}
@@ -80,41 +155,21 @@ models = {'s2s_encode': load_model('s2s', embed_mat, seq_len, 'encode'),
           'att_decode': load_model('att', embed_mat, seq_len, 'decode')}
 
 
-def sample(probs, ind_words, cand):
-    max_probs = np.array(sorted(probs, reverse=True)[:cand])
-    max_probs = max_probs / np.sum(max_probs)
-    max_inds = np.argsort(-probs)[:cand]
-    next_ind = choice(max_inds, p=max_probs)
-    return ind_words[next_ind]
-
-
-def search():
-    pass
-
-
-def predict(text, name):
+def predict(text, name, mode):
     sent1 = re.sub(stop_word_re, '', text.strip())
     seq1 = word2ind.texts_to_sequences([sent1])[0]
     pad_seq1 = pad_sequences([seq1], maxlen=seq_len, padding='pre', truncating='pre')
     encode = map_item(name + '_encode', models)
     state = encode.predict(pad_seq1)
     decode = map_item(name + '_decode', models)
-    sent2 = bos
-    next_word, count = '', 0
-    while next_word != eos and count < max_len:
-        sent2 = ' '.join([sent2, next_word])
-        count = count + 1
-        seq2 = word2ind.texts_to_sequences([sent2])[0]
-        pad_seq2 = pad_sequences([seq2], maxlen=seq_len, padding='post', truncating='post')
-        end = min(count - 1, seq_len - 1)
-        probs = decode.predict([pad_seq2, state])[0][end]
-        next_word = sample(probs, ind_words, cand=5)
-    return sent2[2:]
+    func = map_item(mode, funcs)
+    return func(decode, state, cand=5)
 
 
 if __name__ == '__main__':
     while True:
-        text = input('text: ')
+        # text = input('text: ')
+        text = 'what is your name'
         clean_text = clean(text)
-        print('s2s: %s' % predict(clean_text, 's2s'))
-        print('att: %s' % predict(clean_text, 'att'))
+        print('s2s: %s' % predict(clean_text, 's2s', 'search'))
+        print('att: %s' % predict(clean_text, 'att', 'search'))
